737f50f12f99fa5f6bf890aa67f92dc9b31d620a
CBass92 <140918855+sebastian-92@users.noreply.github.com>
2025-03-05 14:56:06 -0500

    temporary fix

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://tomp.app/',
+    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
