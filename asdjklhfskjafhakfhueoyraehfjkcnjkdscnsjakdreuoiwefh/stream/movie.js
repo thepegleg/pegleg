1c2260dc904c43475a86778351e673c89ad458c3
CBass92 <140918855+sebastian-92@users.noreply.github.com>
2025-03-10 13:25:01 -0400

    switch providers!

--- asdjklhfskjafhakfhueoyraehfjkcnjkdscnsjakdreuoiwefh/stream/movie.js
+++ asdjklhfskjafhakfhueoyraehfjkcnjkdscnsjakdreuoiwefh/stream/movie.js
@@ -2,30 +2,46 @@ let params = new URLSearchParams(window.location.search);
 let movid = params.get("id");
 let streamurl;
 if (movid) {
-  fetch("https://vidapi.cbass92.org/embedsu/" + movid)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      const validsources = [];
-      data.sources[0].files.forEach((source) => {
-        console.log(source);
-        if (source.type) {
-          validsources.push(source);
-        }
-      });
-      if (validsources.length > 0) {
-        streamurl = validsources[0].file;
-        const embed = document.createElement("embed");
-        embed.frameborder = "0";
-        embed.width = "800";
-        embed.height = "450";
-        embed.src = "player/index.html?video=" + streamurl + '&subtitle=' + data.subtitles[0].url ;
-        document.body.appendChild(embed);
-      } else {
-        console.log("No valid sources found. :(");
-        document.write("No valid sources found. :(");
-      }
-    });
+  var anime = prompt("is this an anime? y/n")
+  if (anime == 'y') {
+    const embed = document.createElement("embed");
+    embed.frameborder = "0";
+    embed.width = "100%";
+    embed.height = "100%";
+    embed.src = `https://vidify.top/ani/${movid}`
+    document.body.appendChild(embed);
+  } else {
+    const embed = document.createElement("embed");
+    embed.frameborder = "0";
+    embed.width = "100%";
+    embed.height = "100%";
+    embed.src = `https://vidify.top/embed/movie/${movid}`
+    document.body.appendChild(embed);
+  }
+  // fetch("https://vidapi.cbass92.org/embedsu/" + movid)
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //     const validsources = [];
+  //     data.sources[0].files.forEach((source) => {
+  //       console.log(source);
+  //       if (source.type) {
+  //         validsources.push(source);
+  //       }
+  //     });
+  //     if (validsources.length > 0) {
+  //       streamurl = validsources[0].file;
+  //       const embed = document.createElement("embed");
+  //       embed.frameborder = "0";
+  //       embed.width = "800";
+  //       embed.height = "450";
+  //       embed.src = "player/index.html?video=" + streamurl + '&subtitle=' + data.subtitles[0].url ;
+  //       document.body.appendChild(embed);
+  //     } else {
+  //       console.log("No valid sources found. :(");
+  //       document.write("No valid sources found. :(");
+  //     }
+  //   });
 } else {
   document.write("No movie ID .'-'.");
 }
--- asdjklhfskjafhakfhueoyraehfjkcnjkdscnsjakdreuoiwefh/stream/series.js
+++ asdjklhfskjafhakfhueoyraehfjkcnjkdscnsjakdreuoiwefh/stream/series.js
@@ -4,37 +4,53 @@ let streamurl;
 if (movid) {
   var season = prompt("season?");
   var episode = prompt("episode?");
-  fetch(
-    "https://vidapi.cbass92.org/embedsu/" +
-      movid +
-      "?s=" +
-      season +
-      "&e=" +
-      episode
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      const validsources = [];
-      data.sources[0].files.forEach((source) => {
-        console.log(source);
-        if (source.type == 'hls') {
-          validsources.push(source);
-        }
-      });
-      if (validsources.length > 0) {
-        streamurl = validsources[0].file;
-        const embed = document.createElement("embed");
-        embed.frameborder = "0";
-        embed.width = "800";
-        embed.height = "450";
-        embed.src = "player/index.html?video="+streamurl+"&subtitle=" + data.subtitles[0].url
-        document.body.appendChild(embed);
-      } else {
-        console.log("No valid sources found. :(");
-        document.write("No valid sources found. :(");
-      }
-    });
+  var anime = prompt("is this an anime? y/n")
+  if (anime == 'y') {
+    const embed = document.createElement("embed");
+    embed.frameborder = "0";
+    embed.width = "100%";
+    embed.height = "100%";
+    embed.src = `https://vidify.top/ani/${movid}/${season}/${episode}`
+    document.body.appendChild(embed);
+  } else {
+    const embed = document.createElement("embed");
+    embed.frameborder = "0";
+    embed.width = "100%";
+    embed.height = "100%";
+    embed.src = `https://vidify.top/embed/tv/${movid}/${season}/${episode}`
+    document.body.appendChild(embed);
+  }
+  // fetch(
+  //   "https://vidapi.cbass92.org/embedsu/" +
+  //     movid +
+  //     "?s=" +
+  //     season +
+  //     "&e=" +
+  //     episode
+  // )
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //     const validsources = [];
+  //     data.sources[0].files.forEach((source) => {
+  //       console.log(source);
+  //       if (source.type == 'hls') {
+  //         validsources.push(source);
+  //       }
+  //     });
+  //     if (validsources.length > 0) {
+  //       streamurl = validsources[0].file;
+  //       const embed = document.createElement("embed");
+  //       embed.frameborder = "0";
+  //       embed.width = "800";
+  //       embed.height = "450";
+  //       embed.src = "player/index.html?video="+streamurl+"&subtitle=" + data.subtitles[0].url
+  //       document.body.appendChild(embed);
+  //     } else {
+  //       console.log("No valid sources found. :(");
+  //       document.write("No valid sources found. :(");
+  //     }
+  //   });
 } else {
   document.write("No series ID .'-'.");
 }
